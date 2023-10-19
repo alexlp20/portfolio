123921d9c7ad18aa7bc8dba2b5ee5fb6cb1a9a7f
Alejandro Laguna <alejandrolagunaperez20@gmail.com>
2023-10-19 16:19:09 +0200

    reset

--- components/SkillIcon.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const props = defineProps(['icon', 'text', 'href']);
-</script>
-<template>
-  <div class="flex flex-col w-1/5 items-center text-2xl font-bold gap-5">
-    <Icon :name="icon" class="text-8xl aspect-square" />
-    <NavLink :href="href" :content="text" />
-  </div>
-</template>
